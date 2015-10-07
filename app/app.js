07de1f77d9d0dc7a7a40a1bae14b1aec34688d4d
Nagarjuna Yendluri <nag.yendluri09@gmail.com>
2015-10-07 19:06:36 -0400

    fire slack master

---
+++ app/app.js
@@ -0,0 +1,57 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name angularfireSlackApp
+ * @description
+ * # angularfireSlackApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('angularfireSlackApp', [
+    'firebase',
+    'angular-md5',
+    'ui.router'
+  ])
+  .config(function ($stateProvider, $urlRouterProvider) {
+    $stateProvider
+      .state('home', {
+        url: '/',
+        templateUrl: 'home/home.html',
+        
+      })
+
+      .state('login', {
+        url: '/login',
+        controller: 'AuthCtrl as authCtrl',
+        templateUrl: 'auth/login.html',
+        resolve: {
+          requireNoAuth: function($state, Auth){
+            return Auth.$requireAuth().then(function(auth){
+            $state.go('home');
+            }, function(error){
+              return;
+            });
+          }
+        }
+      })
+
+      .state('register', {
+        url: '/register',
+        controller: 'AuthCtrl as authCtrl',
+        templateUrl: 'auth/register.html',
+        resolve: {
+          requireNoAuth: function($state, Auth){
+            return Auth.$requireAuth().then(function(auth){
+            $state.go('home');
+            }, function(error){
+                return;
+            });
+          }
+        }
+      });
+
+    $urlRouterProvider.otherwise('/');
+  })
+  .constant('FirebaseUrl', 'https://nagslackclone.firebaseio.com/');
